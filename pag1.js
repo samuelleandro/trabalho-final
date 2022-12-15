6b5a20944a677b61c87ec5916b185a11677df9ef
samuelleandro <113246079+samuelleandro@users.noreply.github.com>
2022-12-15 19:20:41 -0300

    Add files via upload

---
+++ pag1.js
@@ -0,0 +1,9 @@
+var apresentar = "Olá, sejam muito bem vindos ao meu trabalho final.  "
+alert (apresentar)
+var nome = prompt("Digite aqui seu nome")
+var nascimento = prompt("Digite seu ano de nascimento")
+var anoatual = '2022'
+var idade = anoatual - nascimento
+
+var completo = `Conforme as informações acima seu nome é ${nome} e a sua idade é de ${idade} anos.`
+alert (completo)
